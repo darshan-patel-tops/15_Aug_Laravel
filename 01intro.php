762b105cc4fa6216b13ab57344980270521a336c
darshan <darshan.patel.tops@gmail.com>
2023-08-15 12:27:20 +0530

    Update

---
+++ 01intro.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First php lecture</title>
+</head>
+<body>
+    <h1>Hello welcome to the class of php</h1>
+    <h3>Dhruvin</h3>
+    <h3>Jatin</h3>
+    <h3>Yash</h3>
+</body>
+</html>
